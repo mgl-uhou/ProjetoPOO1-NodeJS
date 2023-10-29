6a65860303be338337c5015c258d5ac8b06116de
mgl-uhou <miguelferreiralaurentino@gmail.com>
2023-10-29 18:57:52 -0300

    mudando o nome do arquivo

---
+++ classeTarefaComEtiqueta.js
@@ -0,0 +1,15 @@
+import tarefa from "./classeTarefa.js";
+
+class tarefasComEtiqueta extends tarefa {
+  constructor(etiqueta){
+    super();
+    this.etiqueta = "#" + etiqueta;
+  }
+
+  alterar(data, grau, descricao, etiqueta){
+    super.alterar(data, grau, descricao);
+    this.etiqueta = etiqueta;
+  }
+}
+
+export default tarefasComEtiqueta;
