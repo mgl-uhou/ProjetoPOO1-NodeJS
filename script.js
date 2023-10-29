f59477e3829ff112ab43222e5fdd6bfe524cd9c4
mgl-uhou <miguelferreiralaurentino@gmail.com>
2023-10-29 18:53:56 -0300

    Só mexendo num comentário do código

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ import remover from "./funcRemover.js";
 import editarTarefa from "./funcEditarTarefa.js";
 import concluida from "./funcConcluida.js";
 
-window.adicionar = adicionar; // Aqui eu defino a função como Global, para  que o HTML possa chamá-la.
-window.remover = remover; // Aqui eu defino a função como Global, para  que o HTML possa chamá-la.
-window.editarTarefa = editarTarefa; // Aqui eu defino a função como Global, para  que o HTML possa chamá-la.
-window.concluida = concluida; // Aqui eu defino a função como Global, para  que o HTML possa chamá-la.
+/* window.[nome da função] serve para trazê-la para o escopo global do código, tornando possível usá-la pelo index.html */
+
+window.adicionar = adicionar; 
+window.remover = remover; 
+window.editarTarefa = editarTarefa; 
+window.concluida = concluida; 
 
 window.mostrarNova = mostrarNova;
 window.mostrarRemover = mostrarRemover;
